ef83f72c02c07e9410d6f4383a7a7a720965d610
Y4R1-glitch <yarinahomio@gmail.com>
2024-10-23 23:25:01 -0600

    Validacion y manejo de errores

--- App.py
+++ App.py
@@ -5,27 +5,38 @@ import freecurrencyapi
 class App:
 
     def __init__(self, api_key: str):
+        
         self.api = freecurrencyapi.Client(api_key)
         self.currencies = {}
         self.exchanges = {}
 
     def load_data(self):
-        currencies_path = 'data/currencies.json'
+        # Manejo de errores con estructura try
+        try:
+            currencies_path = 'data/currencies.json'
 
-        if os.path.exists(currencies_path):
-            with open(currencies_path, 'r') as f:
-                data = json.load(f)
+            if os.path.exists(currencies_path):
+                with open(currencies_path, 'r') as f:
+                    data = json.load(f)
+                    self.currencies = data['data']
+
+            else:
+                os.mkdir('data')
+
+                data = self.api.currencies()
+                
                 self.currencies = data['data']
 
-        else:
-            os.mkdir('data')
-
-            data = self.api.currencies()
-            self.currencies = data['data']
-
-            with open(currencies_path, 'w') as f:
-                json.dump(data, f)
-                f.close()
+                with open(currencies_path, 'w') as f:
+                    json.dump(data, f)
+                    f.close()
+        # En caso de que la carpeta "data" ya exista, pero no contenga el archivo "currencies", se dara un error de tipo "FileExistsError"
+        # El error "FileExistsError" puede suceder si antes existe un error de "Excpetion"
+        except FileExistsError:
+            print("No se puede crear un archivo que ya existe, por favor borra la carpeta data y vuelve a internarlo")
+        # En caso de no tener conexion wifi o de haber algun error en el token del Api, se dara un error tipo "Exception"
+        except Exception as e:
+            print(f'Hay un error en el servidor, revisa tu token o tu conexion a internet')
 
     def supported_currencies(self):
 
@@ -41,19 +52,28 @@ class App:
                 print(f'{code}: [{currency['symbol_native']}] - {currency['name']}')
 
     def get_exchange_rate(self, base_currency: str, target_currency: str) -> float:
-
+        
         if base_currency not in self.exchanges:
 
             response = self.api.latest(base_currency)
+            if response[0] == 'API returned errors:':
+                raise ValueError("La moneda base es invalida")
             self.exchanges[base_currency] = response['data']
 
         return self.exchanges[base_currency][target_currency]
 
     def convert(self, amount: float, base_currency: str, currencies: list[str]):
+        # Manejo de errores con estructura try
+        try: 
+            for currency in currencies:
 
-        for currency in currencies:
+                result = self.get_exchange_rate(base_currency, currency) * amount
 
-            result = self.get_exchange_rate(base_currency, currency) * amount
-
-            print(f'{self.currencies[currency]['name']} ({self.currencies[currency]['code']}) -> '
-                  f'{result:.2f} {self.currencies[currency]['symbol_native']}')
+                print(f'{self.currencies[currency]['name']} ({self.currencies[currency]['code']}) -> '
+                    f'{result:.2f} {self.currencies[currency]['symbol_native']}')
+        # En caso de que la moneda a la que se quiere convertir no sea valida el tipo de error sea "KeyError"
+        except KeyError:
+            print('La moneda o monedas a las que quieres convertir no estan registradas')
+        # En caso de que la moneda base no sea valida el tipo de error sea "Excepction"
+        except Exception as e:
+            print(f'Ha ocurrido un error: {e.args[1]['base_currency']}')
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from App import App
+import re
 
 # Esta funcion espera la entrada de texto por el usuario y separa por espacios en blanco, retorna una lista con las palabras ingresadas.
 def prompt() -> list[str]:
@@ -6,6 +7,10 @@ def prompt() -> list[str]:
 
     return text.split(' ')
 
+# Funcion que recibe el patron, usando regex, que debe de tener el texto despues del comando "conv" para funcionar
+def validate(text):
+    patron = r"^\d+\s[a-zA-Z]{3}\sto\s[a-zA-Z]{3}(,[a-zA-Z]{3})*$"
+    return re.match(patron, text)
 
 def show_help():
     print('Por hacer: Aqui va un mensaje de ayuda')
@@ -15,6 +20,7 @@ def main():
 
     app = App('fca_live_kLDaZSlSucLjUTwctTPP7XLkTToRMDil2pfJpLuo')
     app.load_data()
+    
 
     while True: # Bucle infinito para simular una terminal.
 
@@ -28,7 +34,15 @@ def main():
         elif command == 'help' or command == '?':
             show_help()
         elif command == "conv":
-            app.convert(float(text_input[1]), text_input[2], text_input[4].split(','))
+            # Llamada a la funcion para la validacion del patron
+            if len(text_input) == 5:
+                chain = text_input[1] + " " + text_input[2] + " " + text_input[3] + " " + text_input[4]
+                if validate(chain):
+                    app.convert(float(text_input[1]), text_input[2].upper(), text_input[4].upper().split(',')) # Convertimos las monedas automaticamente a mayusculas
+                else:
+                    print("La cadena esta mal escrita, intenta con -> conv 'cuantity' 'currency' to 'currency'")
+            else:
+                print("La cadena esta mal escrita, intenta con -> conv 'cuantity' 'currency' to 'currency'")
         elif command == 'supported':
             app.supported_currencies()
         elif command == 'search':
